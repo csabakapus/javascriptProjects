38515d5c119c42d0b34bddc59dd21fc05733f605
csabakapus <kapuscsaba87@gmail.com>
2017-01-25 00:42:58 +0100

    bernardTheLetterCounter.js

---
+++ bernardTheLetterCoutner.js
@@ -0,0 +1,20 @@
+// count the number of letters in a sentence that is asked/said by the user 
+
+function bernardTheLetterCounter() {
+	var sentence = prompt('Please enter a sentence to analyze.');
+	var letter = prompt('Which letter dou you want me to count for you?');
+	var counter = 0;
+
+	
+	for(var i = 0; i < sentence.length; i++) {
+		if (sentence.charAt(i) === letter) {
+			counter++;		
+		}
+	}
+	
+	alert('The letter ' + letter + ' occurs ' + counter + ' times in this sentence.');
+   return;
+}
+
+
+bernardTheLetterCounter();
